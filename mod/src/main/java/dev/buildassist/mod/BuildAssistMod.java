1f4d50421ea154a398c06c29a5732dcdcb8e94a4
Kohxax <koharen0256@gmail.com>
2026-05-25 21:30:07 +0900

    chore: init mod Gradle project with Fabric API dependency

---
+++ mod/src/main/java/dev/buildassist/mod/BuildAssistMod.java
@@ -0,0 +1,16 @@
+package dev.buildassist.mod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BuildAssistMod implements ModInitializer {
+
+    public static final String MOD_ID = "buildassist";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("BuildAssist initialized.");
+    }
+}
